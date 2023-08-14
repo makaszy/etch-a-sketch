05e1b453a38b9cf17d916aede87216b20cf2324b
makaszy <m.a.kaszynski@protonmail.com>
2023-08-14 16:54:30 +0200

    Add createGridItems

---
+++ src/components/views/createGridItems.js
@@ -0,0 +1,10 @@
+/* Create grid-items for the grid */
+
+function createGridItems(amount) {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.classList.add("etch-grid__grid-item");
+    wrapper = document.querySelector(".etch-grid");
+    wrapper.appendChild(div);
+  }
+}
