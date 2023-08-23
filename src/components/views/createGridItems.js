975323376464ce21fe52b8258087e3ef08478e1d
makaszy <m.a.kaszynski@protonmail.com>
2023-08-23 17:01:47 +0200

    Amend createGridItems to include styling for wrapper

--- src/components/views/createGridItems.js
+++ src/components/views/createGridItems.js
@@ -1,10 +1,17 @@
+
+
 /* Create grid-items for the grid */
 
-function createGridItems(amount) {
+function createGridItems(size) {
+  if (+size === NaN) {
+    throw new Error("Value is not a number!")
+  }
+  const amount = +size * +size;
   for (let i = 0; i < amount; i++) {
     const div = document.createElement("div");
     div.classList.add("etch-grid__grid-item");
     wrapper = document.querySelector(".etch-grid");
+    wrapper.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr)`)
     wrapper.appendChild(div);
   }
 }
